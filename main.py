3e7a169419565d0b481949005bab1a7193690409
Giovani Oliani <giovani.oliani@gmail.com>
2018-07-15 12:46:00 -0300

    Add missing commands, simplify operations with methods

--- main.py
+++ main.py
@@ -1,50 +1,77 @@
 from cmd import Cmd
 from collections import deque
 
-class centerOperator(object):
-	state = 'avalilable'
-	call = ""
-	def __init__(self, id):
-		self.id = id
-
 class centerPrompt(Cmd):
-	def initialize(self):
-		self.operators = dict.fromkeys('A','B')
-		self.operators['A'] = centerOperator('A')
-		self.operators['B'] = centerOperator('B')
-		self.available = [self.operators['B'], self.operators['A']]
+	def __init__(self):
+		super().__init__()
+		self.available = ['B', 'A']
+		# Contains Ids of calls waiting to be answered
 		self.waiting = deque()
+		# Contains mappings from busy operators into their calls and vice-versa
 		self.busy = dict()
+		# Contains mapping from ringing calls into their operators and vice-versa
+		self.ringing = dict()
 
-	def do_call(self,id):
-		""" Usage: call <id> """
-		print("Call", id, "received")
+	def dictPopElement(self, mapping, id):
+		ret = mapping.pop(id)
+		del mapping[ret]
+		return ret
+	def dictInsertElement(self, mapping, op_id, call_id):
+		mapping[op_id] = call_id
+		mapping[call_id] = op_id
+
+	def do_call(self, call_id):
+		print("Call", call_id, "received")
 		if len(self.available) == 0:
-			print("Call", id, "is waiting in queue")
-			self.waiting.append(id)
+			self.waiting.append(call_id)
+			print("Call", call_id, "waiting in queue")
 		else:
-			op = self.available.pop()
-			op.state = 'busy'
-			print("Call", id, "answered by operator", op.id)
-			self.busy[id] = op
-	def do_hangup(self, id):
-		""" Usage: hangup <id> """
-		op = self.busy[id]
-		op.state = "available"
-		print("Call", id, "finished and operator", op.id, "available")
-		if len(waiting) > 0:
-			call = self.waiting.popleft()
-			self.busy[id] = op
+			op_id = self.available.pop()
+			self.dictInsertElement(self.ringing, op_id, call_id)
+			print("Call", call_id, "ringing for operator", op_id)
+
+	def do_answer(self, op_id):
+		call_id = self.dictPopElement(self.ringing,op_id)
+		self.dictInsertElement(self.busy, op_id, call_id)
+		print("Call", call_id, "answered by operator", op_id)
+
+
+	def do_reject(self, op_id):
+		call_id = self.dictPopElement(self.ringing, op_id)
+		print("Call", call_id, "rejected by operator", op_id)
+		if len(self.available) == 0:
+			self.available.append(op_id)
+		op_id = self.available.pop()
+		self.dictInsertElement(self.ringing, op_id, call_id)
+		print("Call", call_id, "ringing for operator", op_id)
+
+	def do_hangup(self, call_id):
+		if call_id in self.busy:
+			op_id = self.busy.pop(call_id)
+			del self.busy[op_id]
+			print("Call", call_id, "finished and operator", op_id, "available")
+			if len(self.waiting) > 0:
+				call_id = self.waiting.popleft()
+				self.dictInsertElement(self.ringing, op_id, call_id)
+				print("Call", call_id, "ringing for operator", op_id)
+			else:
+				self.available.append(op_id)
+		elif call_id in self.ringing:
+			op_id = self.dictPopElement(self.ringing, call_id)
+			print("Call", call_id, "missed")
+			if len(self.waiting) > 0:
+				call_id = self.waiting.popleft()
+				self.dictInsertElement(self.ringing, op_id, call_id)
+				print("Call", call_id, "ringing for operator", op_id)
 		else:
-			self.available.append(op)
-	def do_reject(self, id):
-		for call in self.waiting:
-			if call == id:
-				
+			self.waiting.remove(call_id)
+			print("Call", call_id, "missed")
+
+	def do_EOF(self, id):
+	    exit(0)
 
 
 if __name__ == '__main__':
 	prompt = centerPrompt()
-	prompt.initialize()
-	prompt.prompt = '$ '
+	prompt.prompt = ''
 	prompt.cmdloop()
